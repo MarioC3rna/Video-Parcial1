76bed39f6dd4f24515502c293b547a3006b0b008
MARIO FERNANDO CERNA NAJERA <mcernan@miumg.edu.gt>
2024-02-28 23:32:22 -0600

    commit

--- Video-Parcial 1/Program.cs
+++ Video-Parcial 1/Program.cs
@@ -1,42 +1,45 @@
-﻿int opcion, nfactorial;
-double nRaiz, rRaiz;
-
-do
+﻿
+Console.WriteLine("Ingrese el primer número entero:");
+if (!int.TryParse(Console.ReadLine(), out int numero1))
 {
-    Console.WriteLine("Ingresa el numero de la opcion que desea");
-    Console.WriteLine("1. Calcular el factorial de un numero ");
-    Console.WriteLine("2. Calcular la raiz cuadrada de un numero ");
-    Console.WriteLine("3. Salir ");
-    opcion = int.Parse(Console.ReadLine());
-    switch (opcion)
-    {
-        case 1:
-            Console.WriteLine("Ingrese el numero que desea ");
-            nfactorial = int.Parse(Console.ReadLine());
+    Console.WriteLine("Numero Invalido en el ingreso del primer numero");
+    return;
+}
 
-            int factorial = 1;
+Console.WriteLine("Ingrese el segundo número entero:");
+if (!int.TryParse(Console.ReadLine(), out int numero2))
+{
+    Console.WriteLine("Numero Invalido en el ingreso del segundo numero");
+    return;
+}
 
-            for (int i = 1; i <= nfactorial; i++)
-            {
-                factorial *= i;
-            }
-            Console.WriteLine("El resultado es: " + factorial);
+Console.WriteLine("Ingrese el operador matematico (+, -, *, /):");
+string operador = Console.ReadLine();
 
-            break;
-        case 2:
-            Console.WriteLine("Ingrese el numero que desea");
-            nRaiz = double.Parse(Console.ReadLine());
-            rRaiz = Math.Sqrt(nRaiz);
-            Console.WriteLine("La raiz es: " + rRaiz);
-            break;
-        case 3:
-            Console.WriteLine("Saliendo del programa...");
-            break;
-        default:
-            Console.WriteLine("El número ingresado no esta disponible actualmente.");
-            break;
+double resultado;
 
+switch (operador)
+{
+    case "+":
+        resultado = numero1 + numero2;
+        break;
+    case "-":
+        resultado = numero1 - numero2;
+        break;
+    case "*":
+        resultado = numero1 * numero2;
+        break;
+    case "/":
+        if (numero2 == 0)
+        {
+            Console.WriteLine(" No se puede dividir entre cero.");
+            return;
+        }
+        resultado = (double)numero1 / numero2;
+        break;
+    default:
+        Console.WriteLine("Operador inválido.");
+        return;
+}
 
-    }
-
-} while (opcion != 3);
+Console.WriteLine($"El resultado de los numeros {numero1} {operador} {numero2} es: {resultado}");
