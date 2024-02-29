8c21020d9310e62cd4b9e24d12696d089af4f0de
MARIO FERNANDO CERNA NAJERA <mcernan@miumg.edu.gt>
2024-02-28 23:32:56 -0600

    commit

--- Video-Parcial 1/Program.cs
+++ Video-Parcial 1/Program.cs
@@ -1,45 +1,18 @@
 ﻿
-Console.WriteLine("Ingrese el primer número entero:");
-if (!int.TryParse(Console.ReadLine(), out int numero1))
+Console.WriteLine("Tabla de multiplicar");
+Console.WriteLine("---Ingresa un numero aqui---");
+Console.WriteLine("");
+
+if (!int.TryParse(Console.ReadLine(), out int n1))
 {
-    Console.WriteLine("Numero Invalido en el ingreso del primer numero");
+
+    Console.WriteLine("Ingrese un numro entero por favor");
     return;
 }
+Console.WriteLine($"Tabla de multiplicar del {n1}:");
 
-Console.WriteLine("Ingrese el segundo número entero:");
-if (!int.TryParse(Console.ReadLine(), out int numero2))
+for (int i = 1; i <= 10; i++)
 {
-    Console.WriteLine("Numero Invalido en el ingreso del segundo numero");
-    return;
+    Console.WriteLine($"{n1}x{i}={n1 * i}");
 }
 
-Console.WriteLine("Ingrese el operador matematico (+, -, *, /):");
-string operador = Console.ReadLine();
-
-double resultado;
-
-switch (operador)
-{
-    case "+":
-        resultado = numero1 + numero2;
-        break;
-    case "-":
-        resultado = numero1 - numero2;
-        break;
-    case "*":
-        resultado = numero1 * numero2;
-        break;
-    case "/":
-        if (numero2 == 0)
-        {
-            Console.WriteLine(" No se puede dividir entre cero.");
-            return;
-        }
-        resultado = (double)numero1 / numero2;
-        break;
-    default:
-        Console.WriteLine("Operador inválido.");
-        return;
-}
-
-Console.WriteLine($"El resultado de los numeros {numero1} {operador} {numero2} es: {resultado}");
